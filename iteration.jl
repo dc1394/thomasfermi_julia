6c4cfbcb6a5c249974836515b4f29a1a240c584e
dc1394 <hriver2014-bitbucket@memoad.jp>
2021-07-28 15:53:13 +0900

    NormRDの計算法を元に戻した

--- iteration.jl
+++ iteration.jl
@@ -20,22 +20,8 @@ module Iteration
     end
 
     getNormRD(newarray, oldarray, x) = let
-        residual = abs.(newarray .- oldarray)
-    
-        sum = 0.0
-        max = length(residual) - 2
-        dx = x[2] - x[1]
-        
-        # Simpsonの法則で数値積分する
-        @inbounds @simd for i = 1:2:max
-            f0 = sqrt(x[i]) * (residual[i]) ^ 1.5
-            f1 = sqrt(x[i + 1]) * (residual[i + 1]) ^ 1.5 
-            f2 = sqrt(x[i + 2]) * (residual[i + 2]) ^ 1.5
-            
-            sum += (f0 + 4.0 * f1 + f2)
-        end
-        
-        return sum * dx / (12.0 * pi)
+        tmp = newarray .- oldarray 
+        return sqrt(sum(tmp .* tmp))
     end
 
     function iteration!(data, solve_tf_param, solve_tf_val, yarray)
@@ -45,12 +31,12 @@ module Iteration
             newyarray = Solve_TF.solvetf!(iter, solve_tf_param, solve_tf_val, yarray)
             normrd = getNormRD(newyarray, yarray, solve_tf_val.node_x_glo)
             
+            @printf("Iteration # %d: NormRD = %.15f\n", iter + 1, normrd)
             if normrd <= data.iteration_criterion
                 convergence_flag = true
                 break
             end
             
-            @printf("Iteration # %d: NormRD = %.15f\n", iter + 1, normrd)
             yarray = simple_mixing(data, newyarray, yarray)
         end
 
